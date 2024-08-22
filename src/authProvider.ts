9fa133dcaf5a969913365f386e1a19440b22f807
Dave Shoup <dave.shoup@gmail.com>
2024-08-21 20:18:55 -0400

    add progress notification and comments

--- src/authProvider.ts
+++ src/authProvider.ts
@@ -17,12 +17,15 @@ export class AuthProvider implements vscode.AuthenticationProvider {
   }
 
   constructor(private readonly context: vscode.ExtensionContext) {
+    // watch for changes to the secret, which is persistent and available across windows/workspaces
     context.secrets.onDidChange(async (e) => {
       if (e.key === STORAGE_KEY) {
         const secret = await context.secrets.get(STORAGE_KEY);
         if (secret) {
+          // user logged in (secret was added)
           this._onDidChangeSessions.fire({ added: [TEST_SESSION], removed: [], changed: [] });
         } else {
+          // user logged out (secret was removed)
           this._onDidChangeSessions.fire({ added: [], removed: [TEST_SESSION], changed: [] });
         }
       }
@@ -30,12 +33,24 @@ export class AuthProvider implements vscode.AuthenticationProvider {
   }
 
   async createSession(scopes: string[]): Promise<vscode.AuthenticationSession> {
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    vscode.window.withProgress(
+      {
+        location: vscode.ProgressLocation.Notification,
+        title: "Logging in...",
+      },
+      async () => {
+        // NOTE: this is where you would implement your auth flow and take advantage of the scopes,
+        // as well as implement any additional steps like MFA, URI handling, etc.
 
-    await this.context.secrets.store(STORAGE_KEY, JSON.stringify(TEST_SESSION));
-    this._onDidChangeSessions.fire({ added: [TEST_SESSION], removed: [], changed: [] });
+        // simulate some network latency / time to complete an auth flow
+        await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    vscode.window.showInformationMessage("Session created");
+        await this.context.secrets.store(STORAGE_KEY, JSON.stringify(TEST_SESSION));
+        this._onDidChangeSessions.fire({ added: [TEST_SESSION], removed: [], changed: [] });
+
+        vscode.window.showInformationMessage("Session created");
+      },
+    );
 
     return TEST_SESSION;
   }
